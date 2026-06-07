63c02eaaab62fa4935002b73941096b47e208404
Qureen-Bhandari07 <qureenbhandari376@gmail.com>
2026-06-07 14:54:55 +0545

    Added question2

---
+++ question2.py
@@ -0,0 +1,4 @@
+#Write a program that calculates the area of a rectangle. Add appropriate comments explaining each line of code.
+l = float(input('Enter the length of rectangle, l : '))
+b = float(input("Enter the breadth of rectangle, b: "))
+area = ("Area of rectangle = ",l*b)
