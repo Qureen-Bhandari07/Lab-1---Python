2dbe5c7de9b5a60882b3591d246da814ce225421
[gitit] <qureenbhandari@gmail.com>
2026-06-07 14:19:11 +0545

    Added question1

---
+++ question1.py
@@ -0,0 +1,4 @@
+# Write a program that prints your name, age, and city using three different print statements.
+print ('Hi! I am Qureen Bhandari.')
+print(" I am 20 years old.")
+print(' I live in Lubhu.')
