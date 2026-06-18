04fe2ac0674b609aa5726a4188aac42932576893
Qureen-Bhandari07 <qureenbhandari376@gmail.com>
2026-06-18 09:39:06 +0545

    Sample codes

---
+++ Lab2/SimpleReflexAgent.py
@@ -0,0 +1,35 @@
+def simple_reflex_agent(location, status):
+    # location: 'A' or 'B', status: 'Dirty' or 'Clean'
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Move to B"
+    elif location == "B":
+        return "Move to A"
+    else:
+        return "NoOp"
+
+
+print("********** Simple Reflex Agent **********\n")
+print("Program by: [Your Name]")
+print("Roll no: [Your Roll No]\n")
+
+
+print("\nThe percept sequence is (A, Dirty)")
+action = simple_reflex_agent("A", "Dirty")
+print("Action:", action)
+
+
+print("\nThe percept sequence is (A, Clean)")
+action = simple_reflex_agent("A", "Clean")
+print("Action:", action)
+
+
+print("\nThe percept sequence is (B, Dirty)")
+action = simple_reflex_agent("B", "Dirty")
+print("Action:", action)
+
+
+print("\nThe percept sequence is (B, Clean)")
+action = simple_reflex_agent("B", "Clean")
+print("Action:", action)
